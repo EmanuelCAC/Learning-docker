8bf48c134e3dfb6c617da4619e716d28deccd19a
EmanuelCAC <emanuelc279@gmail.com>
2024-05-15 10:39:38 -0300

    fixed live update

--- 02.react-app/src/App.js
+++ 02.react-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          {`Hello ${process.env.REACT_APP_NAME}`}
+          {`Hello ${process.env.REACT_APP_NAME} teste`}
         </p>
         <a
           className="App-link"
